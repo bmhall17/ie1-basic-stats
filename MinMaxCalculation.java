0f312c034e9deed77b1784b151534562423f6ca3
bmhall17 <bmhall0826@gmail.com>
2024-10-01 18:22:35 -0400

    Updates code in MinMaxCalculation.java to comply with Java conventions

--- MinMaxCalculation.java
+++ MinMaxCalculation.java
@@ -1,6 +1,17 @@
-public MinMaxCalculation(int a, int b) {
-  int max = Math.max(a, b);
-  int min = Math.min(a, b);
-
-  return {max, min};
+public class MinMaxCalculation {
+  public static void main(String[] args) {
+    int maxVal = Integer.MAX_VALUE;
+    int minVal = Integer.MIN_VALUE;
+    for(int i = 0; i < args.length; i++) {
+      int curVal = Integer.parseInt(args[i]);
+      if (curVal < minVal) {
+        minVal = curVal;
+      }
+      if (curVal > maxVal) {
+        maxVal = curVal;
+      } 
+    }
+    System.out.println("Max: " + maxVal);
+    System.out.println("Min: " + minVal);
+  }
 }
